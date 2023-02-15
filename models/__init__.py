a83580a33e9e8481dec955bc17c069f01bffecd2
nasasiraarnold2 <nasasiraarnold20@gmail.com>
2023-02-14 17:31:48 -0800

    All files

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/Python3
+"""File Storage instance creation
+"""
+
+
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""City class inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class city inheriting from BaseModel
+    """
+
+    state_id = ""
+    name = ""
+
